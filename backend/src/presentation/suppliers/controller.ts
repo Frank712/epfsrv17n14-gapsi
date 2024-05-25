38a12bd5b751d370aa1b194e891d5703500887d0
Francisco Lino <franksuriel_7@hotmail.com>
2024-05-25 12:14:59 -0600

    ADD use cases to controllers

--- backend/src/presentation/suppliers/controller.ts
+++ backend/src/presentation/suppliers/controller.ts
@@ -1,22 +1,62 @@
 import { Response, Request } from "express";
+import { CreateSupplierDto } from "../../domain/dtos/suppliers/create-supplier.dto";
+import { SupplierRepository } from "../../domain/repositories/supplier.repository";
+import { CreateSupplier } from "../../domain/use-cases/suppliers/create-supplier.use-case";
+import { CustomError } from "../../domain/errors/custom.error";
+import { DeleteSupplierDto } from "../../domain/dtos/suppliers/delete-supplier.dto";
+import { DeleteSupplier } from "../../domain/use-cases/suppliers/delete-supplier.use-case copy";
+import { GetAllSuppliersDto } from "../../domain/dtos/suppliers/get-all-suppliers.dto";
+import { GetAllSuppliers } from "../../domain/use-cases/suppliers/get-all-suppliers";
 
 export class SupplierController {
+  constructor(private readonly supplierRepository: SupplierRepository) {}
+
   private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.errorCode).json({ error: error.message });
+    }
     return res.status(500).json({ error: "Internal server error" });
   };
 
   //:TODO Get list of suppliers
   getSuppliers = (req: Request, res: Response) => {
-    res.json("Get all suppliers");
+    const [error, getAllSupplierDto] = GetAllSuppliersDto.create(req.body);
+
+    if (error) {
+      res.status(400).json({ error });
+    }
+
+    new GetAllSuppliers(this.supplierRepository)
+      .execute(getAllSupplierDto!)
+      .then((data) => res.json(data))
+      .catch((error) => this.handleError(error, res));
   };
 
   //:TODO Add a supplier
   createSupplier = (req: Request, res: Response) => {
-    res.json("Add supplier");
+    const [error, createSupplierDto] = CreateSupplierDto.create(req.body);
+
+    if (error) {
+      res.status(400).json({ error });
+    }
+
+    new CreateSupplier(this.supplierRepository)
+      .execute(createSupplierDto!)
+      .then((data) => res.json(data))
+      .catch((error) => this.handleError(error, res));
   };
 
   //:TODO Delete a supplier
   deleteSupplier = (req: Request, res: Response) => {
-    res.json("Delete supplier");
+    const [error, deleteSupplierDto] = DeleteSupplierDto.create(req.body);
+
+    if (error) {
+      res.status(400).json({ error });
+    }
+
+    new DeleteSupplier(this.supplierRepository)
+      .execute(deleteSupplierDto!)
+      .then((data) => res.json(data))
+      .catch((error) => this.handleError(error, res));
   };
 }
