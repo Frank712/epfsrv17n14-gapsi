4ffc5f4937e0c1383e8bd2eba4cf45d10f87aaef
Francisco Lino <franksuriel_7@hotmail.com>
2024-05-25 12:13:44 -0600

    FIX name get all supplier use case

--- backend/src/domain/use-cases/suppliers/get-all-suppliers.ts
+++ backend/src/domain/use-cases/suppliers/get-all-suppliers.ts
@@ -2,11 +2,11 @@ import { SupplierEntity } from "../../entities/supplier.entity";
 import { GetAllSuppliersDto } from "../../dtos/suppliers/get-all-suppliers.dto";
 import { SupplierRepository } from "../../repositories/supplier.repository";
 
-interface GetAllSupplierUseCase {
+interface GetAllSuppliersUseCase {
   execute(getAllSuppliersDto: GetAllSuppliersDto): Promise<SupplierEntity[]>;
 }
 
-export class GetAllSupplier implements GetAllSupplierUseCase {
+export class GetAllSuppliers implements GetAllSuppliersUseCase {
   constructor(private supplierRepository: SupplierRepository) {}
 
   execute(getAllSuppliersDto: GetAllSuppliersDto): Promise<SupplierEntity[]> {
